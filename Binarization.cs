e4d75e1f317e2338ec05959f4c60108dd3eff349
date17 <date17@ezweb.ne.jp>
2019-10-08 16:15:17 +0900

    correction possword

--- Binarization.cs
+++ Binarization.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,49 +33,3 @@ namespace ImageChange
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-//二値化
-namespace ImageChange
-{
-    class Binarization
-    {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> origin/master
-        public int[,] Binary_click(byte[,] data, int siki)
-        {
-            const int SIZE = 256;
-            int[,] kekka_binary = new int[SIZE, SIZE];
-
-            for(int i = 0; i < SIZE; i++)
-            {
-                for(int j = 0; j < SIZE; j++)
-                {
-                    if(data[i,j] < siki)
-                    {
-                        kekka_binary[i, j] = 0;
-                    }
-                    else
-                    {
-                        kekka_binary[i, j] = 255;
-                    }
-                }
-            }
-
-            return kekka_binary;
-        }
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 514173b9de7afb00bd1171dbfcc34dced302085c
->>>>>>> origin/master
-    }
-}
->>>>>>> origin/master
